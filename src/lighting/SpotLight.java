aab804a3ca8c858392d221c9ecd12429f7d53c5f
Shifra Wexler <swexler@g.jct.ac.il>
2022-05-22 17:04:14 +0300

    almost finished 6 and fixed the previous problem

--- src/lighting/SpotLight.java
+++ src/lighting/SpotLight.java
@@ -3,10 +3,10 @@ import primitives.Color;
 import primitives.Point;
 import primitives.Vector;
 
-public class SpotLight extends PointLight implements LightSource {
+public class SpotLight extends PointLight {
 
-    private double radius;
-    private double narrowBeam=0;
+    private final Vector direction;
+    private double narrowBeam=0d;
 
     /**
      * @param intensity
@@ -18,39 +18,16 @@ public class SpotLight extends PointLight implements LightSource {
         this.direction = direction.normalize();
     }
 
-    final Vector direction;
+    @Override
+    public Color getIntesity(Point point) {
+        double max=Math.max(0, getL(point).dotProduct(direction));
+        return  super.getIntesity(point).scale(max);
 
-    /**
-     * Constructor of Spotlight which receives four params
-     *
-     * @param intensity light intensity
-     * @param position  Light starts location
-     * @param direction direction of the light
-     * @param radius
-     */
-    public SpotLight(Color intensity, Point position, Vector direction, double radius) {
-        super(intensity, position);
-        this.direction = direction.normalize();
-        this.radius = radius;
-    }
-
-    public Color getIntensity(Point p) {
-        double projection = direction.dotProduct(getL(p));
-
-        double factor = Math.max(0, projection);
-
-        Color pointIntensity = super.getIntesity(p);
-
-        return pointIntensity.scale(factor);
     }
 
     public SpotLight setNarrowBeam(double narrowBeam) {
         this.narrowBeam = narrowBeam;
         return  this;
-    }
-
-    public double getNarrowBeam() {
-        return narrowBeam;
     }
 }
 
